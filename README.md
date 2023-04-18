77a5cf496225fe1ea9d6c0d1b07e9680a6c380c1
mikeyCos <109037087+mikeyCos@users.noreply.github.com>
2023-04-17 17:17:22 -0700

    Added placholder images, flex and grid displays implemented, and stylized header.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # art-portfolio
+17 APR 2023: Imported placeholder images, applied flex display for mobile, implemented grid display for gallery section, and set a minimum width for desktop view.  
--- javascript.js
+++ javascript.js
@@ -1,1 +1,18 @@
-console.log('hi');
+// Article helped me figure out current scoll location
+    // https://dev.to/ljcdev/scroll-to-top-button-in-vanilla-js-beginners-2nc
+let topButton = document.getElementById('top-btn');
+topButton.addEventListener('click', goTop);
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.documentElement.scrollTop > document.documentElement.clientHeight) {
+        topButton.style.display = "block";
+    } else {
+        topButton.style.display = "none";
+    }
+}
+
+function goTop()  {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
